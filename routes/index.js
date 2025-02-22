f25e7bb2f264e7f0ecdd7847a205af0c0483b54e
Rickyth36 <rickythoudam123@gmail.com>
2025-02-22 16:48:50 +0530

    user authentication,profile edit done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,73 @@
 var express = require('express');
 var router = express.Router();
+const userModel = require("../routes/users");
+const passport = require('passport');
+const localStrategy = require("passport-local");
+const upload = require('./multer');
 
-/* GET home page. */
+passport.use(new localStrategy(userModel.authenticate()));
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index',{nav:false});
 });
+router.get('/profile',isLoggedIn, async function(req, res, next) {
+  const user = await userModel.findOne({
+    username:req.session.passport.user
+  });
+  res.render("profile",{user,nav:true});
+});
+
+
+router.post('/fileupload',isLoggedIn,upload.single("image"), async function(req, res, next) {
+  const user = await userModel.findOne({
+    username:req.session.passport.user
+  })
+  user.profileImage = req.file.filename;
+  await user.save();
+  res.redirect("/profile");
+});
+
+
+
+
+router.get('/register', function(req, res, next) {
+  res.render('register',{nav:false});
+});
+
+router.post("/register",function(req,res,next){
+  const {username,email,conatact} = req.body;
+  const userData = new userModel({
+    username,
+    email,
+    conatact
+  })
+  userModel.register(userData,req.body.password).
+  then(function(){
+    passport.authenticate("local")(req,res,function(){
+      res.redirect("/profile");
+    })
+  })
+})
+
+router.post("/login",passport.authenticate("local",{
+  successRedirect:"/profile",
+  failureRedirect:"/"
+}),function(req,res,next){
+
+});
+
+router.get("/logout",function(req,res,next){
+  req.logOut(function(err){
+    if(err) {return next(err);}
+    res.redirect("/");
+  })
+})
+
+function isLoggedIn(req,res,next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/")
+}
 
 module.exports = router;
